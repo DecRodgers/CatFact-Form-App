5641513dd54e050f675044b33e348bce3cd9f668
Declan R. <78768579+DecRodgers@users.noreply.github.com>
2024-03-15 02:22:18 +0000

    model added to proj

---
+++ CatFact Form App/Models/CatFact.cs
@@ -0,0 +1,16 @@
+﻿namespace CatFactAPI.Model.CatFact;
+
+public class CatFact() { 
+    public string? fact {  get; set; }
+    public int length { get; set; }
+
+    public string ReturnFact() 
+    {
+        return fact;    
+    }
+    public int ReturnFactLength()
+    {
+        return length;
+    }
+
+}
